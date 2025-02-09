a199701344fef4e4721226cc57972f49147d99a0
Blake Bradford <b@blrfam.win>
2025-02-08 21:41:44 -0500

    first commit

---
+++ ts/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": "error",
+  },
+};
---
+++ ts/webpack.common.js
@@ -0,0 +1,38 @@
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+    devtool: "source-map",
+    entry: {
+        index: "./src/code/forms/index.ts",
+    },
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: "maps/[name].js.map",
+        path: path.resolve(__dirname, "../Webresources/js"),
+        library: ["bebe", "[name]"],
+        libraryTarget: "var",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ESLintPlugin({
+            fix: true,
+            extensions: ["ts", "tsx"],
+            lintDirtyModulesOnly: true,
+            failOnError: true,
+        }),
+    ],
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+};
